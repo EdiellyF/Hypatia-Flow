c06354b4ec8ead6cae4ecdba6ccfe9e107d85645
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-06-21 12:53:29 -0300

    feat: adiciona rotas de disciplina

---
+++ backend/backend/src/routes/disciplinaRouter.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import {authMiddleware} from '../middleware/auth.js';
+import { DisciplinaController } from '../controllers/disciplinaController.js';
+import { DisciplinaRepository } from '../repository/disciplinaRepository.js';
+import { DisciplinaService } from '../services/disciplinaService.js';
+import { UserService } from '../services/userService.js';
+import { UserRepository } from '../repository/userRepository.js';
+import { EmailService } from '../services/emailService.js';
+
+
+const userRepository = new UserRepository();
+const disciplinaRepository = new DisciplinaRepository();
+
+const emailService = new EmailService();
+const userService = new UserService(userRepository, emailService);
+const disciplinaService = new DisciplinaService(disciplinaRepository, userService);
+
+
+const disciplinaController = new DisciplinaController(disciplinaService);
+
+const disciplinaRouter = Router();
+
+disciplinaRouter.post('/', authMiddleware, (req, res) => disciplinaController.createDisciplina(req, res));
+
+disciplinaRouter.delete('/:id', authMiddleware,(req, res) => disciplinaController.deleteDisciplinaById(req,res))
+
+export {disciplinaRouter};
