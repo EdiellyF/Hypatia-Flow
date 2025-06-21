915ed8e180f3deb2b645df9c52c4c456a8a1cf34
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-06-21 12:52:48 -0300

    feat: implementa controller de disciplina

---
+++ backend/backend/src/controllers/disciplinaController.js
@@ -0,0 +1,76 @@
+import { errorHandler } from "../middleware/errorHandler.js";
+
+export class DisciplinaController{
+    #disciplinaService;
+    
+
+    constructor(disciplinaService) {
+       this.#disciplinaService = disciplinaService;
+    }
+
+   async createDisciplina(req,res) {
+    try {
+       const {nome, descricao } = req.body; 
+        if (!nome){
+            return new errorHandler(res, 400, "Nome é obrigatório").send();
+        }
+
+        
+        const disciplinaExistente = await this.#disciplinaService.findDisciplinaByName(nome,  req.user.id);
+        if (disciplinaExistente) {
+            return new errorHandler(res, 400, "Disciplina já existe").send();
+        }
+
+        const disciplina = await this.#disciplinaService.createDisciplina({userId: req.user.id, nome, descricao});
+        console.log(disciplina)
+
+
+        return res.status(201).json({message: "Disciplina criada com sucesso", disciplina});
+
+       
+
+    }catch(error){
+        console.error('Erro ao criar disciplina:', error);
+        return new errorHandler(res, 500, `Erro ao criar disciplina: ${error.message}`).send();
+    }
+     
+
+   }
+
+
+   async deleteDisciplinaById(req,res){
+        try {
+            const {id} = req.params;
+            const disciplina = await this.#disciplinaService.findDisciplinaById(id);
+
+            
+
+            if (!disciplina) {
+                return new errorHandler(res, 404, "Disciplina não encontrada").send();
+            }
+
+            if(disciplina.idUsuarioCriador != req.user.id){
+                return new errorHandler(res, 403, "Acesso negado!!!").send();
+            }
+
+
+            const deleteDisciplina = await this.#disciplinaService.deleteDisciplinaById(id);
+
+
+                if(deleteDisciplina) {
+                    return res.status(204).json();
+                }
+
+                
+
+            }
+
+            catch(error){
+               console.error('Erro ao deletar disciplina:', error);
+               return new errorHandler(res, 500, `Erro ao deletar disciplina: ${error.message}`).send();
+            }
+
+
+    }
+
+}
